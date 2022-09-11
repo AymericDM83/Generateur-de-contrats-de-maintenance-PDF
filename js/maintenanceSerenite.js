6c4c92476021de8872275486bb44282b452bce82
Chill83 <aymericmaggio@gmail.com>
2022-09-12 00:33:12 +0200

    Générateur contrat simple ok

---
+++ js/maintenanceSerenite.js
@@ -0,0 +1,45 @@
+
+
+// Tableau système maintenance sérénité
+const maintenanceSereniteProductsTab = [
+    "1 Mono split maintenance sérénité",
+    "1 Bi-split maintenance sérénité",
+    "1 Tri-split maintenance sérénité",
+    "1 Quadri-split maintenance sérénité",
+    "1 Split gainable maintenance sérénité",
+    "1 PAC résidentiel Air/Eau basse température maintenance sérénité",
+    "1 PAC résidentiel Air/Eau haute température maintenance sérénité",
+    "1 Ballon ECS Thermodynamique maintenance sérénité",
+    "1 PAC Piscine maintenance sérénité",
+    "Jusqu'à 3 unités intérieures maintenance sérénité",
+    "De 3 à 6 unités intérieures maintenance sérénité",
+    "2 splits gainable maintenance sérénité"
+];
+// Tableau prix maintenance sérénité
+const maintenanceSerenitePriceTab = [
+    164.99, 188.56, 215.35, 253.91, 231.42, 243.20, 286.06, 123.21, 181.06, 242.14, 286.05, 318.20
+]
+// Tableau deplacement supp sérénité
+const deplacementSuppTabSerenite = [
+    "Non",
+    "Zone B rayon 50-70 Kms maintenance sérénité",
+    "Zone C rayon 70-100 Kms maintenance sérénité",
+    "Zone D rayon 100-120 Kms maintenance sérénité"
+]
+// tableau prix déplacement supp sérénité
+const deplacementSuppPriceTabSerenite = [
+    0, 46, 95, 142
+
+]
+
+
+// Tableau options supplémentaires serenite
+const optionSuppTabSerenite = [
+    "Non",
+    "Option Piscine sur PAC maintenance sérénité",
+    "Option PAC > à 25 Kw maintenance sérénité"
+]
+// Tableau prix options supplementaires serenite
+const optionSuppPriceTabSerenite = [
+    0, 59.99, 54.64
+]
